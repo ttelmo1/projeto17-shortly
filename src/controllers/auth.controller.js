5a79dc83b23e8d610699690df9cd2f78c85e1adb
Telmo Junior <telmobsj@gmail.com>
2023-03-03 09:10:13 -0300

    feat: signin route

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,41 @@
+import bcrypt from "bcrypt";
+import { v4 as uuidv4 } from "uuid";
+import { db } from "../database/db.js";
+
+export async function signin(req, res) {
+    const { email, password } = req.body;
+
+    try{
+        const { rows } = await db.query(
+            `
+            SELECT * FROM users WHERE email = $1`,
+            [email]
+        );
+
+        const user = rows[0];
+
+        if(!user){
+            return res.status(401).send("Invalid credentials");
+        }
+
+        const isPasswordValid = bcrypt.compareSync(password, user.password);
+
+        if(!isPasswordValid){
+            return res.status(401).send("Invalid credentials");
+        }
+
+        const token = uuidv4();
+
+        await db.query(
+            `
+            INSERT INTO sessions (token, "userId") VALUES ($1, $2)`,
+            [token, user.id]
+        );
+
+        res.send(token);
+        
+    }
+    catch(err){
+        res.status(500).send(err.message);
+    }   
+}
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
+import { signin } from "../controllers/auth.controller.js";
 import { createUser } from "../controllers/users.controller.js";
 
 const router = Router();
 
 router.post("/signup", createUser);
+router.post("/signin", signin);
 
 export default router;
